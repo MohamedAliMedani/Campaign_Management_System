570b2497c6e1e5b8e2f039006367cec977b2bf78
Mohamed Ali <mohamed.ali.medani@gmail.com>
2024-12-31 23:15:05 +0200

    AddValidationMail

--- Campaign_Management_System/Src/Features/Subscriber/Commands/PostSubscriber/PostSubscriberCommand.cs
+++ Campaign_Management_System/Src/Features/Subscriber/Commands/PostSubscriber/PostSubscriberCommand.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
 using MediatR;
 using Src.Helpers;
 
 namespace Campaign_Management_System.Src.Features.Subscriber.Commands.PostSubscriber
 {
     public class PostSubscriberCommand:IRequest<ResponseDTO>
-    {
+    {    
+        [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address format.")]
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string SubscriptionStatus { get; set; }
